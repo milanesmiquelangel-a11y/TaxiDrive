a4b71dd355a92b75bcb9ff48581b8a584b98676c
milanesmiquelangel-a11y <milanes.miquelangel@gmail.com>
2026-09-25 11:14:34 +0500

    Implement driver activation at 500 KZT for 24 hours

--- TaxiDrive_v2_beta/app/src/main/java/kz/taxidrive/app/model/User.kt
+++ TaxiDrive_v2_beta/app/src/main/java/kz/taxidrive/app/model/User.kt
@@ -1,28 +1,20 @@
 package kz.taxidrive.app.model
 
 data class User(
-
     val uid: String = "",
-
     val nombre: String = "",
-
     val apellidos: String = "",
-
     val telefono: String = "",
-
-    val tipo: String = "",      // pasajero | conductor | administrador
-
+    val tipo: String = "",
     val aprobado: Boolean = false,
-
     val marca: String = "",
-
     val modelo: String = "",
-
     val color: String = "",
-
     val anio: String = "",
-
     val matricula: String = "",
-
-    val licencia: String = ""
+    val licencia: String = "",
+    val trialUntil: Long = 0L,
+    val activationUntil: Long = 0L,
+    val activationPending: Boolean = false,
+    val activationAmount: Long = 500L
 )
